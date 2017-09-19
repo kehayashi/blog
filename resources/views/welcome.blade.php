8b528aa89816cb54bbafa5daafc455d7cd401a2d
kehayashi <kehayashi@hotmail.com>
2017-09-19 16:16:29 -0300

    icone pequeno aba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title><img src="{!! secure_asset('img/ICONE-PEQUENO.png') !!}"/> Inova Direito | Sua fonte online de conteúdo grátis</title>
+    <title>Inova Direito | Sua fonte online de conteúdo grátis</title>
     <link href="{{ secure_asset("/vendor/bootstrap/css/bootstrap.min.css") }}" rel="stylesheet">
     <link href="{{ secure_asset("/vendor/font-awesome/css/font-awesome.min.css") }}" rel="stylesheet" type="text/css">
     <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
