b4cb22cc76b34cb0648936c1b3f57680d37eb7a9
kehayashi <kehayashi@hotmail.com>
2017-09-19 09:49:28 -0300

    migrations

--- database/migrations/lead.php
+++ database/migrations/lead.php
@@ -4,16 +4,10 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class leadTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('lead', function (Blueprint $table) {
+class leadTable extends Migration {
+
+    public function up() {
+        Schema::create('lead', function ($table) {
             $table->increments('lead_id');
             $table->string('nome');
             $table->string('sobrenome');
@@ -21,14 +15,10 @@ class leadTable extends Migration
             $table->string('empresa');
             $table->string('cargo');
             $table->string('data_hora');
+            $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('lead');
--- database/migrations/posts.php
+++ database/migrations/posts.php
@@ -6,26 +6,17 @@ use Illuminate\Database\Migrations\Migration;
 
 class postsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('posts', function ($table) {
             $table->increments('post_id');
             $table->text('titulo');
             $table->text('descricao');
             $table->string('data_hora');
+            $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('posts');
