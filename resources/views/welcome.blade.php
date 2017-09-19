3a0f4c07fb18d46c5894ff27898679f66c6dd4f3
kehayashi <kehayashi@hotmail.com>
2017-09-19 16:30:20 -0300

    teste godaddy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,8 +22,8 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <title>Inova Direito | Sua fonte online de conteúdo grátis</title>
-    <link href="{{ secure_asset("/vendor/bootstrap/css/bootstrap.min.css") }}" rel="stylesheet">
-    <link href="{{ secure_asset("/vendor/font-awesome/css/font-awesome.min.css") }}" rel="stylesheet" type="text/css">
+    <link href="{{ secure_asset("vendor/bootstrap/css/bootstrap.min.css") }}" rel="stylesheet">
+    <link href="{{ secure_asset("vendor/font-awesome/css/font-awesome.min.css") }}" rel="stylesheet" type="text/css">
     <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
     <link href="{{ secure_asset("css/clean-blog.min.css") }}" rel="stylesheet">
