21d865a90787fabd5671cc90daf790dc22781627
kehayashi <kehayashi@hotmail.com>
2017-09-20 11:29:31 -0300

    correcoes testes usuarios

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,22 +3,21 @@
 @section('conteudo')
 
 <div class="row">
-  <div class="col-md-12 mx-auto">
+  <div class="col-md-10 mx-auto">
     <div class="post-preview">
         <h3 class="post-title text-center">
           {{ $post[0] }}
           {{ $post->titulo }}
         </h3>
-          <pre style="font">
+          <p>
             {{ $post->descricao }}
-          </pre>
+          </p>
       <p class="post-meta text-center">Posted by
         <a href="#">#modo_beta</a>
         {{ $post->data_hora }} </p> <a href="/">Voltar</a>
     </div>
     <hr>
   </div>
-
 </div>
 
 @stop
